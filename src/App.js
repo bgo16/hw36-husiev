ab287de7174b1f338a1dd276e61f6c3d3aef86d6
Bohdan Husiev <bgusev77@gmail.com>
2023-09-14 17:15:11 +0300

    jlwejvl

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <NavBar />
+      <CenterContainer />
+    </>
   );
 }
 
-export default App;
+function Header() {
+  const headerStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    background: 'grey'
+  };
+
+  return <h1 style={headerStyle}>Header</h1>;
+}
+
+function NavBarElement({ count }) {
+  const navElementStyle = {
+    listStyle: 'none',
+    margin: '5px',
+    background: 'green'
+  };
+
+  return (
+    <li style={navElementStyle}>
+      <a href={count + '+item'}>{count + ' item'}</a>
+    </li>
+  );
+}
+
+function NavBar() {
+  const navStyle = {
+    float: 'left',
+    background: 'lightgreen'
+  };
+
+  return (
+    <nav style={navStyle}>
+      <aside>
+        <h2>Navigation bar</h2>
+        <ul>
+          <NavBarElement count='first' />
+          <NavBarElement count='second' />
+          <NavBarElement count='third' />
+        </ul>
+      </aside>
+    </nav>
+  );
+}
+
+function CenterContainer() {
+  const CenterContainerStyle = {
+    marginLeft: '20%',
+    padding: '1rem',
+    background: 'lightblue'
+  };
+
+  return <div style={CenterContainerStyle}>Some text content</div>;
+}
